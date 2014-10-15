71194c93d886079fdc54cd40ca27d5c6d742f8a5
Imanuel Ulbricht <knerdprod@live.de>
2014-10-15 18:59:51 +0200

    added first ticketprovider

--- Ticketmanager/config.js
+++ Ticketmanager/config.js
@@ -1,1 +1,6 @@
-﻿exports.DefaultDomain = 'garbsen1'
+﻿/*
+ * the domain that should be used if no domain is provided for login
+ */
+exports.DefaultDomain = 'garbsen1'
+exports.CouchDBServerHost = 'localhost'
+exports.CouchDBServerPort = '5984'
---
+++ Ticketmanager/persistence/ticketProvider.js
@@ -0,0 +1,40 @@
+﻿var config = require('../config.js')
+var cradle = require('cradle')
+
+ticketprovider = function () {
+    this.connection = new (cradle.Connection)(host, port, {
+        cache: true,
+        raw: false
+    })
+    this.db = this.connection.database('tickets')
+}
+ticketprovider.prototype.setup = function (callback) {
+    this.db.save('_design/tickets', {
+        free: {
+            map: function (doc) {
+                if (!doc.assigned)
+                    emit(doc.id, doc);
+            }
+        },
+        unprioritised: {
+            map: function (doc) {
+                if (!doc.priority) {
+                    emit(doc.id, doc)
+                }
+            }
+        }
+    })
+}
+ticketprovider.prototype.findAllFree = function (callback) {
+    this.db.view('tickets/free', function (error, result) {
+        if (error) {
+            callback(error)
+        } else {
+            var docs = []
+            result.forEach(function (row) {
+                docs.push(row)
+            })
+            callback(null, docs);
+        }
+    })
+}
