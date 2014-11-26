fd6f33a4cc73e82ac924137cd95e1050a8cf9546
Imanuel Ulbricht <knerdprod@live.de>
2014-11-26 13:01:19 +0100

    we can update finally
    not sure how good this option is, but it works :)

--- Serpentes/app.js
+++ Serpentes/app.js
@@ -9,15 +9,17 @@ var path = require('path')
 var fs = require('fs')
 var expless = require('less-middleware')
 var locale = require('locale')
-var localizer = require('./localizer.js')
-var settings = require('./routes/settings.js')
-var supported = ["de", "de_DE", "en"]
-
+var childprocess = require('child_process')
 var exphbs = require('express-handlebars')
 var hbshelper = require('handlebars-helpers')
 
+var localizer = require('./localizer.js')
+var settings = require('./routes/settings.js')
 var dbsetup = require('./persistence/databaseSetup.js')
+var WorkerProvider = require('./persistence/workerProvider.js').WorkerProvider
 var routesetup = require('./routes/routesSetup.js')
+
+var supported = ["de", "de_DE", "en"]
 
 var app = express()
 var hbs = exphbs.create({
@@ -134,6 +136,30 @@ app.use(function (req, res, next) {
             return opts.fn(this)
         }
     }
+    io.on('connection', function (socket) {
+        socket.on('start', function (data) {
+            if (req.session['isAdmin']) {
+                socket.emit('log', { message: req.localize('updating serpentes') })
+                var process = childprocess.spawn('git' , ['pull', 'origin', 'feature/i-like-updates'])
+                process.stdout.on('data', function (data) {
+                    console.log(data.toString())
+                    socket.emit('log', { message: data.toString('UTF-8') });
+                })
+                process.stderr.on('data', function (data) {
+                    console.log(data.toString())
+                    socket.emit('log', { message: data.toString('UTF-8') });
+                })
+                process.on('exit', function (code, signal) {
+                    console.log('exited with code ' + code)
+                    socket.emit('log', { message: 'exit with code ' + code })
+                    process.kill()
+                })
+            } else {
+                socket.emit('log', req.localize('only admins can update'))
+            }
+        })
+    })
+    
     req.localize = function (key) { return __localize(key, req) }
     next()
 })
@@ -167,8 +193,12 @@ if ('development' == app.get('env')) {
 
 var options = { pfx: fs.readFileSync('localhost.pfx') }
 
-https.createServer(options, app).listen(app.get('port'), function () {
+var server = https.createServer(options, app)
+var io = require('socket.io')(server)
+server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 })
+
 dbsetup.setup()
 settings.initializedesign(function () { })
+var workerprovider = new WorkerProvider()
--- Serpentes/routes/admin.js
+++ Serpentes/routes/admin.js
@@ -1,32 +1,26 @@
-﻿var childprocess = require('child_process')
-var server = require('http').createServer(function (req, res) {
-    res.writeHead(200, { 'content/type': 'text/plain' })
-    res.end('okay')
-})
-var io = require('socket.io').listen(server)
-
-/*
+﻿/*
  * GET admin/update
  */
 exports.update = function (req, res) {
-    res.render('admin/updates/index', { title: req.localize('update serpentes') })
+    res.render('admin/updates/index', { title: req.localize('update serpentes'), server: req.host })
 }
 
 /*
  * POST admin/update
  */
 exports.updatePost = function (req, res) {
-    server.listen(23400)
-    io.sockets.on('connection', function (socket) {
-        var process = childprocess.spawn('git', ['pull origin feature/i-like-update'], { cwd: '.' })
-        process.stdout.on('data', function (data) {
-            console.log(data.toString())
-            socket.emit('log', { message: data.toString('UTF-8') });
-        })
-        process.stderr.on('data', function (data) {
-            console.log(data.toString())
-            socket.emit('log', { message: data.toString('UTF-8') });
-        })
+    var result = ''
+    var process = childprocess.spawn('git', ['pull origin feature/i-like-update'], { cwd: '.' })
+    process.stdout.on('data', function (data) {
+        console.log(data.toString())
+        result += data.toString()
     })
-    res.render('admin/updates/updating', { title: req.localize('updating serpentes'), server: req.host })
+    process.stderr.on('data', function (data) {
+        console.log(data.toString())
+        result += data.toString()
+    })
+    process.on('close', function (data) {
+        result += data.toString()
+        res.render('admin/updates/updating', { title: req.localize('update serpentes'), data: result })
+    })
 }
