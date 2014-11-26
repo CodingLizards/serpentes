877bf57196cdc0cfaac74c62eb2065f6a5a8b198
Imanuel Ulbricht <knerdprod@live.de>
2014-11-26 18:53:21 +0100

    install script didn't work with clone
    so we imitated clone and now it works just fine :)

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
-git clone https://github.com/CodingLizards/serpentes.git .
+git init .
+git remote add -f -t master -m master origin https://github.com/CodingLizards/serpentes.git
+git checkout master
 call npm install
 call npm install -g nodemon
 start http://localhost:1337/
