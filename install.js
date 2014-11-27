bc0eb73f7baa96d2b9cb2ad1ec7ed2f5ac292be1
Imanuel Ulbricht <knerdprod@live.de>
2014-11-27 15:02:26 +0100

    install is fixed finally
    the solution is, we need to copy the file

--- install.js
+++ install.js
@@ -3,31 +3,32 @@ var fs = require('fs')
 var path = require('path')
 
 var parseForm = function (data) {
-    var reg = /-----------------------------.+(\r\n|\r|\n)Content-Disposition: form-data; name="/
-    var sections = data.split(reg)
-    var result = {}
-    for (sec in sections) {
-        var values = sections[sec].split(/\b"(\r\n|\r|\n)(\r\n|\r|\n)/)
-        if (/certificate"; filename/.test(values[0])) {
-            var key = values[0].replace(/"; filename=".*/i, '')
-            var value = values[3].replace(/Content-Type: application\/x-pkcs12(\r\n|\n|\r)+/i, '').replace(/-----------------------------.+--(\r\n|\r|\n)/, '')
-            result[key] = new Buffer(value, 'ascii').toString()
-        } else if (values[3]) {
-            var key = values[0]
-            var value = values[3].replace(/(\r\n|\r|\n)/, '')
-            result[key] = value
+    var parts = 'set ' + decodeURIComponent(data.replace(/&/g, '\r\nset '))
+    return parts
+}
+
+var checkFile = function (res) {
+    res.writeHead(200, { "content-type": "text/html" })
+    fs.exists(path.join(__dirname, 'server.p12'), function (exists) {
+        if (exists) {
+            res.end(fs.readFileSync(path.join(__dirname, 'installsuccess.html')))
+            console.log('it is supposed to crash now')
+            response.end()
+        } else {
+            res.writeHead(500, { "content-type": "text/html" })
+            var page = fs.readFileSync(path.join(__dirname, 'installcertmissing.html'), { encoding: 'utf8' }).replace('{{folder}}', __dirname)
+            console.log(typeof page)
+            res.end(page)
         }
-    }
-    return result
+    })
 }
 
 var server = http.createServer(function (req, res) {
-    console.log(req.method)
     if (req.method == 'GET') {
         res.writeHead(200, { "content-type": "text/html" })
         res.end(fs.readFileSync(path.join(__dirname, 'install.html')))
     } else if (req.method == 'POST') {
-        req.setEncoding('base64')
+        console.log(req.url)
         var body = ''
         req.on('data', function (data) {
             body += data
@@ -36,32 +37,21 @@ var server = http.createServer(function (req, res) {
             if (data) {
                 body += data
             }
-            var parameter = parseForm(new Buffer(body, 'base64').toString('ascii'))
-            var startbat = ''
-            
-            for (var p in parameter) {
-                if (p == 'certificate') {
-                    fs.writeFile(path.join(__dirname, 'server.p12'), parameter[p], function (err) {
-                        console.error(err)
-                        console.error('please copy the file to "' + __dirname + '" and name it server.p12')
-                    })
-                } else {
-                    startbat += 'set ' + p + '=' + parameter[p] + '\r\n'
-                }
+            if (req.url == '/') {
+                var startbat = parseForm(body)
+                startbat += '\r\nnodemon app.js'
+                fs.writeFile(path.join(__dirname, 'start.bat'), startbat, function (err) {
+                    if (err) {
+                        console.log(err)
+                        res.writeHead(500, { "content-type": "text/plain" })
+                        res.end('an error occured, try again please')
+                    } else {
+                        checkFile(res)
+                    }
+                })
+            } else if (req.url == '/ready') {
+                checkFile(res)
             }
-            startbat += 'nodemon app.js'
-            fs.writeFile(path.join(__dirname, 'start.bat'), startbat, function (err) {
-                if (err) {
-                    console.log(err)
-                    res.writeHead(500, { "content-type": "text/plain" })
-                    res.end('an error occured, try again please')
-                } else {
-                    res.writeHead(200, { "content-type": "text/html" })
-                    res.end(fs.readFileSync(path.join(__dirname, 'installsuccess.html')))
-                    console.log('it is supposed to crash now')
-                    response.end()
-                }
-            })
         })
     }
 })
