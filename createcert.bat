70fb0c4bcf2f9e921308fc71d89d7744a2172be1
Imanuel Ulbricht <ulbricht@compra.de>
2016-10-17 14:40:55 +0200

    updated install script

--- createcert.bat
+++ createcert.bat
@@ -1,3 +1,3 @@
-set PATH=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin
-makecert -sv localhost.pvk -n "cn=localhost:1337" localhost.cer -b 10/15/2014 -e 01/01/2015 -r
-pvk2pfx -pvk localhost.pvk -spc localhost.cer -pfx localhost.pfx
+set PATH=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Binmakecert -sv server.pvk -n "cn=localhost:1337" server.cer -b 10/15/2014 -e 01/01/2020 -r
+makecert -sv server.pvk -n "cn=localhost:1337" server.cer -b 10/15/2014 -e 01/01/2020 -r
+pvk2pfx -pvk server.pvk -spc server.cer -pfx server.p12
